39a0f7a92e77f275bd1480a870f6e69a50632d8c
Graham F. Scott <gfscott@gmail.com>
2020-03-25 20:52:27 -0400

    programmatic test cases

--- test.js
+++ test.js
@@ -4,15 +4,38 @@ const extractVideoId = require('./lib/extractMatches.js');
 const buildEmbedCodeString = require('./lib/buildEmbed.js');
 
 const validStrings = [
-  {type: 'Ideal case', str: '<p>https://www.youtube.com/watch?v=hIs5StN8J-0</p>'},
-  {type: 'Without https', str: '<p>http://www.youtube.com/watch?v=hIs5StN8J-0</p>'},
-  {type: 'Without protocol', str: '<p>www.youtube.com/watch?v=hIs5StN8J-0</p>'},
-  {type: 'Without www', str: '<p>youtube.com/watch?v=hIs5StN8J-0</p>'},
-  {type: 'Without watch?v=', str: '<p>youtube.com/hIs5StN8J-0</p>'},
+  {type: 'With standard', str: 'https://www.youtube.com/watch?v=hIs5StN8J-0'},
+  {type: 'With http', str: 'http://www.youtube.com/watch?v=hIs5StN8J-0'},
+  {type: 'Without protocol', str: 'www.youtube.com/watch?v=hIs5StN8J-0'},
+  {type: 'With https, without www', str: 'https://youtube.com/watch?v=hIs5StN8J-0'},
+  {type: 'With http, without www', str: 'http://youtube.com/watch?v=hIs5StN8J-0'},
+  // note this isn’t actually a valid YouTube URL, but works with the plugin
+  {type: 'Without watch?v=', str: 'youtube.com/hIs5StN8J-0'},
+  {type: 'With youtu.be', str: 'youtu.be/hIs5StN8J-0'},
+  {type: 'With arbitrary params', str: 'https://www.youtube.com/watch?v=hIs5StN8J-0&foo=bar&baz'},
 ]
 
 validStrings.forEach(function(obj){
-  test(obj.type, t => {
-    t.truthy(patternPresent(obj.str));
+  test(`${obj.type} ideal case`, t => {
+    let idealCase = `<p>${obj.str}</p>`;
+    t.truthy(patternPresent(idealCase));
+  });
+  test(`${obj.type} with links`, t => {
+    let withLinks = `<p><a href="">${obj.str}</a></p>`;
+    t.truthy(patternPresent(withLinks));
+  });
+  test(`${obj.type} with whitespace`, t => {
+    let withWhitespace = `<p>
+      ${obj.str}
+    </p>`;
+    t.truthy(patternPresent(withWhitespace));
+  });
+  test(`${obj.type} with links and whitespace`, t => {
+    let withLinksAndWhitespace = `<p>
+      <a href="">
+        ${obj.str}
+      </a>
+    </p>`;
+    t.truthy(patternPresent(withLinksAndWhitespace));
   });
 })
